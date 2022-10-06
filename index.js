f0a971a3f4d44300b95b1dd119c33732debd6444
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-06 22:13:15 +0530

    Post Router Added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(morgan("common"));
 // Routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(3001, () => {
   console.log("Hello Bharat, Backend is running successfully");
