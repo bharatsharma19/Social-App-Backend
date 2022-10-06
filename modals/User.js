fb26f30adfe66a06a9d06468532a52c139e18043
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-06 21:10:10 +0530

    Modal Added

---
+++ modals/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 24,
+      unique: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      max: 64,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      min: 6,
+      max: 64,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    following: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,30 @@
 const router = require("express").Router();
+const User = require("../modals/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req, res) => {
-  res.send("Auth");
+// Register
+router.post("/register", async (req, res) => {
+  try {
+    // Encrypting Password
+    const salt = await bcrypt.genSalt(16);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Posting Details
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Creating New User
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error : ", error);
+  }
 });
+
+// Login
+router.post("/login", (req, res) => {});
 
 module.exports = router;
