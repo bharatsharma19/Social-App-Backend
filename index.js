389c24e877792ad23038dd8dd322adca7c227fa2
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-06 20:42:38 +0530

    Database Connected

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to database Successfully");
+});
+
+
+
+app.listen(3001, () => {
+  console.log("Hello Bharat, Backend is running successfully");
+});
+
+// Bharat Sharma
