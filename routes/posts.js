9a5d759f719f3d65c3d94dd5655a08e0d8fccf01
Bharat Sharma <82795831+Bharatsharma19@users.noreply.github.com>
2022-10-16 17:59:35 +0530

    Api Updated

--- routes/posts.js
+++ routes/posts.js
@@ -79,11 +79,9 @@ router.get("/:id", async (req, res) => {
 });
 
 // Get timeline Post
-router.get("/timeline/all", async (req, res) => {
-  let postArray = [];
-
+router.get("/timeline/:userId", async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
 
     const userPosts = await Post.find({ userId: currentUser._id });
 
@@ -93,7 +91,7 @@ router.get("/timeline/all", async (req, res) => {
       })
     );
 
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     res.status(500).json(error);
   }
